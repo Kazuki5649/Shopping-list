382ade7ee8b1d0f363f2b642f70148b3dfc7f7b7
Kazuki5649 <kazuki_yamagishi@jp.honda>
2026-08-27 16:29:29 +0900

    Update service worker to version 2

--- service-worker.js
+++ service-worker.js
@@ -1,72 +1,105 @@
-const cacheName = "yamagishi-shopping-v1";
+const cacheName =
+    "yamagishi-shopping-v2";
 
 const filesToCache = [
     "./",
     "./index.html",
-    "./manifest.json",
-    "./icon-192.png",
-    "./icon-512.png"
+    "./manifest.json"
 ];
 
-self.addEventListener("install", function (event) {
-    event.waitUntil(
-        caches
-            .open(cacheName)
-            .then(function (cache) {
-                return cache.addAll(filesToCache);
-            })
-    );
-
-    self.skipWaiting();
-});
-
-self.addEventListener("activate", function (event) {
-    event.waitUntil(
-        caches
-            .keys()
-            .then(function (cacheNames) {
-                return Promise.all(
-                    cacheNames.map(
-                        function (currentCache) {
-                            if (
-                                currentCache !==
-                                cacheName
-                            ) {
-                                return caches.delete(
-                                    currentCache
-                                );
-                            }
-                        }
-                    )
-                );
-            })
-    );
-
-    self.clients.claim();
-});
-
-self.addEventListener("fetch", function (event) {
-    event.respondWith(
-        fetch(event.request)
-            .then(function (response) {
-                const responseCopy =
-                    response.clone();
-
-                caches
-                    .open(cacheName)
-                    .then(function (cache) {
-                        cache.put(
-                            event.request,
-                            responseCopy
+self.addEventListener(
+    "install",
+    function (event) {
+        event.waitUntil(
+            caches
+                .open(cacheName)
+                .then(
+                    function (cache) {
+                        return cache.addAll(
+                            filesToCache
                         );
-                    });
+                    }
+                )
+        );
 
-                return response;
-            })
-            .catch(function () {
-                return caches.match(
-                    event.request
-                );
-            })
-    );
-});
+        self.skipWaiting();
+    }
+);
+
+self.addEventListener(
+    "activate",
+    function (event) {
+        event.waitUntil(
+            caches
+                .keys()
+                .then(
+                    function (
+                        cacheNames
+                    ) {
+                        return Promise.all(
+                            cacheNames.map(
+                                function (
+                                    currentCache
+                                ) {
+                                    if (
+                                        currentCache !==
+                                        cacheName
+                                    ) {
+                                        return caches.delete(
+                                            currentCache
+                                        );
+                                    }
+                                }
+                            )
+                        );
+                    }
+                )
+        );
+
+        self.clients.claim();
+    }
+);
+
+self.addEventListener(
+    "fetch",
+    function (event) {
+        if (
+            event.request.method !==
+            "GET"
+        ) {
+            return;
+        }
+
+        event.respondWith(
+            fetch(event.request)
+                .then(
+                    function (response) {
+                        const responseCopy =
+                            response.clone();
+
+                        caches
+                            .open(cacheName)
+                            .then(
+                                function (
+                                    cache
+                                ) {
+                                    cache.put(
+                                        event.request,
+                                        responseCopy
+                                    );
+                                }
+                            );
+
+                        return response;
+                    }
+                )
+                .catch(
+                    function () {
+                        return caches.match(
+                            event.request
+                        );
+                    }
+                )
+        );
+    }
+);
